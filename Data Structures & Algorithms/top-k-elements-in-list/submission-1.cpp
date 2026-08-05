40b25dd37e30bb404fabbccf08a1da7c8eaa227d
Sachin <sachin001823@gmail.com>
2026-08-05 18:36:41 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,36 @@
+#include <ranges>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> topKFrequent(const std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> frequency;
+        frequency.reserve(nums.size());
+
+        for (const auto num : nums) {
+            ++frequency[num];
+        }
+
+        std::vector<std::vector<int>> buckets(nums.size() + 1);
+
+        for (const auto& [num, count] : frequency) {
+            buckets[count].push_back(num);
+        }
+
+        std::vector<int> result;
+        result.reserve(k);
+
+        for (const auto& bucket : buckets | std::views::reverse) {
+            for (const auto num : bucket) {
+                result.push_back(num);
+
+                if (result.size() == static_cast<std::size_t>(k)) {
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+};

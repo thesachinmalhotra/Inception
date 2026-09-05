cac01a7c71bdabe86d80104a083aaa732772546f
Sachin Malhotra <sachin001823@gmail.com>
2026-09-05 12:41:25 +0530

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(const std::vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (const int price : prices) {
+            minPrice = std::min(minPrice, price);
+
+            const int profit = price - minPrice;
+            maxProfit = std::max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+};

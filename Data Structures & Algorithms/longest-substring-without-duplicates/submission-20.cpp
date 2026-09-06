0bc7f4913ebae059ae851518af9453e9617bac53
Sachin Malhotra <sachin001823@gmail.com>
2026-09-06 12:53:04 +0530

    Add: longest-substring-without-duplicates - submission-20

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-20.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string& s) {
+        std::array<int, 128> lastSeen;
+        lastSeen.fill(-1);
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < static_cast<int>(s.size()); ++right) {
+            unsigned char character = s[right];
+
+            if (lastSeen[character] >= left) {
+                left = lastSeen[character] + 1;
+            }
+
+            lastSeen[character] = right;
+            maxLength = std::max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};

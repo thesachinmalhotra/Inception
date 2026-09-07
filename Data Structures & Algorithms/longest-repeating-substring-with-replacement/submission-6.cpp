d890346f2b6a1ea7e202f1d2403738dec22c7a97
Sachin Malhotra <sachin001823@gmail.com>
2026-09-07 18:39:48 +0530

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    int characterReplacement(const std::string& s, int k) {
+        std::array<int, 26> frequency{};
+
+        int left = 0;
+        int maxLength = 0;
+        int maxFrequency = 0;
+
+        for (int right = 0; right < static_cast<int>(s.size()); ++right) {
+            const int index = s[right] - 'A';
+            ++frequency[index];
+
+            maxFrequency = std::max(maxFrequency, frequency[index]);
+
+            while (right - left + 1 - maxFrequency > k) {
+                --frequency[s[left] - 'A'];
+                ++left;
+            }
+            maxLength = std::max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+};

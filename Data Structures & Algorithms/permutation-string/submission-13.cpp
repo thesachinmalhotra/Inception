c35548f6341a86074050c87ee8152c111977bd95
Sachin Malhotra <sachin001823@gmail.com>
2026-09-08 20:53:02 +0530

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    bool checkInclusion(const string& s1, const string& s2) {
+        if (s1.size() > s2.size()) return false;
+
+        array<int, 26> need{}, window{};
+
+        for (char c : s1) ++need[c - 'a'];
+
+        for (int i = 0; i < s1.size(); ++i) ++window[s2[i] - 'a'];
+
+        for (int left = 0, right = s1.size(); right < s2.size(); ++left, ++right) {
+            if (window == need) return true;
+            
+            --window[s2[left] - 'a'];
+            ++window[s2[right] - 'a'];
+            }
+            return window == need;
+        }
+};

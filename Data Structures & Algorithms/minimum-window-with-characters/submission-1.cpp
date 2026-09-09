34652951062bfbbec9e0fd350a262459378e919e
Sachin Malhotra <sachin001823@gmail.com>
2026-09-09 15:27:07 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+   public:
+    string minWindow(const string& s, const string& t) {
+        if (t.empty() || t.size() > s.size()) return ""; 
+
+        array<int, 128> need{};
+        array<int, 128> window{};
+
+        int required = 0;
+
+        for (unsigned char c : t) {
+            if (need[c] == 0) ++required;
+            ++need[c];
+        }
+
+        int formed = 0;
+        int left = 0;
+        int bestStart = 0;
+        int bestLength = INT_MAX;
+
+        for (int right = 0; right < static_cast<int>(s.size()); ++right) {
+            unsigned char c = s[right];
+
+         if (++window[c] == need[c]) ++formed;
+
+                while (formed == required) {
+                    const int length = right - left + 1;
+
+                    if (length < bestLength) {
+                        bestLength = length;
+                        bestStart = left;
+                    }
+
+                    unsigned char leftChar = s[left];
+
+                    if (--window[leftChar] < need[leftChar]) --formed;
+
+                    ++left;
+                }
+            }
+            return bestLength == INT_MAX ? "" : s.substr(bestStart, bestLength);
+        }
+    };

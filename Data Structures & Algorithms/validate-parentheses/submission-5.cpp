28ca96af25c2bf3e5bb685be92928d2aa1749700
Sachin Malhotra <sachin001823@gmail.com>
2026-09-11 15:06:59 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool isValid(const std::string& s) {
+        std::stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};

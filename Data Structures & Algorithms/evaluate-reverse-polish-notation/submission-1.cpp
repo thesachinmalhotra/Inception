5420719ef324936b0972ca06eba165a4477237a8
Sachin Malhotra <sachin001823@gmail.com>
2026-09-13 18:25:09 +0530

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dfs(vector<string>& tokens) {
+        string token = tokens.back();
+        tokens.pop_back();
+
+        if (token != "+" && token != "-" &&
+             token != "*" && token != "/")
+        {
+            return stoi(token);
+        }
+
+        int right = dfs(tokens);
+        int left = dfs(tokens);
+
+        if (token == "+") {
+            return left + right;
+        } else if (token == "-") {
+            return left - right;
+        } else if (token == "*") {
+            return left * right;
+        } else {
+            return left / right;
+        }
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        return dfs(tokens);
+    }
+};

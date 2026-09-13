c1392733d8bb36d65b443c97e38d8e58656969c1
Sachin Malhotra <sachin001823@gmail.com>
2026-09-13 19:10:45 +0530

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(const std::vector<int>& temperatures) {
+        std::stack<int> st;
+        std::vector<int> result(temperatures.size(), 0);
+
+        for (int right = 0; right < static_cast<int>(temperatures.size()); ++right) {
+            while (!st.empty() && temperatures[right] > temperatures[st.top()]) {
+
+                int left = st.top();
+                st.pop();
+
+                result[left] = right - left;
+            }
+            st.push(right);
+        }
+        return result;
+    }
+};

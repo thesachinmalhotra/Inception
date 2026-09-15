311cfa45ec78c41c5812d168b1a78a1ebe28da05
Sachin Malhotra <sachin001823@gmail.com>
2026-09-15 18:25:36 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    int carFleet(int target, const std::vector<int>& position, const std::vector<int>& speed) {
+        std::vector<pair<int, int>> cars;
+        cars.reserve(position.size());
+
+        for (int i = 0; i < position.size(); ++i) {
+            cars.push_back({position[i], speed[i]});
+        }
+
+        std::sort(cars.begin(), cars.end(),
+                  [](const auto& a, const auto& b) { return a.first > b.first; });
+
+        int fleets = 0;
+        double fleetTime = 0;
+
+        for (const auto& [pos, spd] : cars) {
+            double time = static_cast<double>(target - pos) / spd;
+
+            if (time > fleetTime) {
+                ++fleets;
+                fleetTime = time;
+            }
+        }
+        return fleets;
+    }
+};

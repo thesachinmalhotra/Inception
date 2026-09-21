fda18d5083c5a18eedafb11d03529df8d616d95c
Sachin Malhotra <sachin001823@gmail.com>
2026-09-21 17:32:49 +0530

    Add: time-based-key-value-store - submission-9

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-9.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+   private:
+    std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> keystore;
+
+   public:
+    TimeMap() {}
+    void set(std::string key, std::string value, int timestamp) {
+        keystore[key].emplace_back(timestamp, value);
+    }
+
+    std::string get(std::string key, int timestamp) {
+        auto& values = keystore[key];
+
+        int left = 0;
+        int right = values.size() - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (values[mid].first <= timestamp) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result == -1 ? "" : values[result].second;
+    }
+};

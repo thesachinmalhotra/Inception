296d4b7c547ae70b645928c453c67609b6ed5ecf
Sachin Malhotra <sachin001823@gmail.com>
2026-09-24 18:25:46 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+   public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+
+        ListNode* previous = nullptr;
+
+        while (second) {
+            ListNode* next = second->next;
+            second->next = previous;
+            previous = second;
+            second = next;
+        }
+
+        ListNode* first = head;
+        second = previous;
+
+        while (second) {
+        ListNode* firstNext = first->next;
+        ListNode* secondNext = second->next;
+
+        first->next = second;
+        second->next = firstNext;
+
+        first = firstNext;
+        second = secondNext;
+      }
+    }
+};
